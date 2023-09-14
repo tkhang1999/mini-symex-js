7c1d507758c5c5dfef264e8cd48c6df52989e729
Khang Le <tkhang1999@gmail.com>
2023-09-14 15:56:25 -0700

    add comments

--- src/symbolic.js
+++ src/symbolic.js
@@ -1,18 +1,14 @@
-import {
-  solver,
-  counter,
-  readCounter,
-  args,
-  constraints,
-  z3Ctx,
-} from "./utils";
+import { solver, readCounter, args, constraints, z3Ctx } from "./utils";
 import { spawnSync } from "child_process";
 
-export const symbolicBool = (sym, isChild) => {
+export const symbolicBool = (expr, shouldNegate) => {
   let result = undefined;
 
-  if (!isChild) {
-    if (counter === args.length + 1) {
+  if (shouldNegate !== "true") {
+    // if this condition is encountered for the first time based on args length,
+    // a child process will be spawned to negate this condition to take another path.
+    if (shouldNegate === undefined) {
+      // child process to negate "expr"
       const childArgs = [...args, "true"];
       const child = spawnSync(
         process.argv[0],
@@ -23,20 +19,20 @@ export const symbolicBool = (sym, isChild) => {
       );
       console.log(child.stdout);
     }
-    solver.assert(sym);
-    result = true;
-    constraints.push(`assume ${sym}`);
-  } else {
-    solver.assert(z3Ctx.mkNot(sym));
-    result = false;
-    constraints.push(`assume (not ${sym})`);
-  }
-
-  if (counter === args.length + 1) {
+    // current process to not negate "expr"
     args.push("false");
+    solver.assert(expr);
+    result = true;
+    constraints.push(`assume ${expr}`);
+  } else {
+    // alternative path taken by spawned child process
+    solver.assert(z3Ctx.mkNot(expr));
+    result = false;
+    constraints.push(`assume (not ${expr})`);
   }
 
   if (!solver.check()) {
+    // if given constraints is unsatisfiable, stop processing further
     console.log(`Path unreachable: ${constraints}`);
     process.exit(0);
   }
@@ -44,6 +40,11 @@ export const symbolicBool = (sym, isChild) => {
   return result;
 };
 
+/**
+ * SymbolicInt is a wrapper class to represent integer symbolic values using Z3 expression.
+ *
+ * Operator overloading is needed for symbolic execution.
+ */
 export class SymbolicInt {
   id;
   expr;
@@ -56,17 +57,15 @@ export class SymbolicInt {
   [Symbol.for("===")](other) {
     return symbolicBool(
       z3Ctx.mkEq(this.expr, other.getZ3Expr()),
-      args[readCounter()] === "true",
+      args[readCounter()],
     );
-    // symbolicBool(Z3.Eq(z3.Int.const(this.id), z3.Int.const(other.id)));
   }
 
   [Symbol.for("!==")](other) {
     return symbolicBool(
       z3Ctx.mkNot(z3Ctx.mkEq(this.expr, other.getZ3Expr())),
-      args[readCounter()] === "true",
+      args[readCounter()],
     );
-    // symbolicBool(z3.Not(z3.Eq(z3.Int.const(this.id), z3.Int.const(other.id))));
   }
 
   getZ3Expr() {
--- src/utils.js
+++ src/utils.js
@@ -4,12 +4,13 @@ export const z3Ctx = new Z3.Context();
 
 export const solver = new Z3.Solver(z3Ctx, false, []);
 
-export let counter = 0;
+let counter = 0;
 
 export const readCounter = () => {
   return counter++;
 };
 
+// optional arguments where args[i] indicates whether the i-th boolean expression should be negated
 export const args = process.argv.slice(2);
 
 export const constraints = [];
